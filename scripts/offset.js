4b4f908c0878b41246c2624bb7fca39fd7f613ab
Vincent Liuss <liusvinv@gmail.com>
2022-10-27 16:55:28 +0700

    Fix the offset alignment on the offseteffect

--- scripts/offset.js
+++ scripts/offset.js
@@ -17,10 +17,36 @@ window.addEventListener("mousemove", (e)=> {
 
 function applyEffect(element, offsetX, offsetY){
 
+
     // console.log(element);
-    offsetX *= 'offset' in element.dataset ? element.dataset.offset: moveFactor;
-    offsetY *= 'offset' in element.dataset ? element.dataset.offset: moveFactor;
+    offsetX *= 'offset' in element.dataset ? element.dataset.offset : moveFactor;
+    offsetY *= 'offset' in element.dataset ? element.dataset.offset : moveFactor;
     
+    offsetFactor = 'offset' in element.dataset ? element.dataset.offset : 0;
+
+    if('align' in element.dataset){
+        // Animate on all direction equally by reducing the offset
+        if(element.dataset.align == "true"){
+            offsetX = 10 + Math.abs(offsetX) - Math.abs(offsetFactor) / 2;
+            offsetY = 10 + Math.abs(offsetY) - Math.abs(offsetFactor) / 2 ;
+
+        }
+
+        if('reverse' in element.dataset){
+
+            // Reverse the direction of the animation
+            if(element.dataset.reverse == "true"){
+                offsetX *= -1;
+                offsetY *= -1;
+            }
+            console.log("align center" + offsetX + " -- " + offsetY);
+
+        }
+
+    }
+    
+
+
     element.animate(
         {
             transform:`translate(${offsetX}px, ${offsetY}px)`
